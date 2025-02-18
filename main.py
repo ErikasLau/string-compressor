ea35b9ba1b5c773a4acf63cfe00f53fd73dbc913
Erikas Lauzadis <elauzadis@gmail.com>
2025-02-18 21:37:38 +0200

    Added a string decompression function.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 def string_compress(string):
     print("Compressing string", string)
 
+    # Remove unnecessary spaces
     string = string.strip()
 
     compressed_string = ""
@@ -45,13 +46,54 @@ def string_compress(string):
 # "a3b2" → "aaabb"
 # "a1b1c1" → "abc"
 # "" → ""
-def string_decompress():
-    print("Decompressing string")
+def string_decompress(string):
+    print("Decompressing string", string)
+
+    # Remove unnecessary spaces
+    string = string.strip()
+
+    decompressed_string = ""
+    string_length = len(string)
+
+    # Ideally the information should contain letter and number. In example: a3
+    # Check if compression has this format
+    if string_length % 2 != 0:
+        print("The string dosen't have a correct format (letter and number): a3")
+        return decompressed_string
+
+    # Iterate over string by one letter compression information
+    for char_index in range(0, string_length, 2):
+        # Also validate correct format of letter and number: a3
+
+        # Find information about one letter
+        current_letter = string[char_index]
+        current_number = string[char_index + 1]
+
+        # Check if compressed string contains defined format
+        if not current_letter.isalpha() or not current_number.isnumeric():
+            print("The string dosen't have a correct format (letter and number): a3")
+            print("Decompressed string", decompressed_string)
+            return decompressed_string
+
+        # Convert char to int
+        current_number = int(current_number)
+
+        # Add letter defined number of times to decompressed string
+        for _ in range(current_number):
+            decompressed_string += current_letter
+
+    print("Decompressed string", decompressed_string)
+    return decompressed_string
+
 
 def main():
     string_compress("aaabb")
     string_compress("abc")
     string_compress("")
+
+    string_decompress("a3b2")
+    string_decompress("a1b1c1")
+    string_decompress("")
 
 if __name__ == '__main__':
     main()
