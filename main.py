f2ad013718e555ccfb339bf83430bead9a4c2ba3
Erikas Lauzadis <elauzadis@gmail.com>
2025-02-18 22:23:23 +0200

    Fix compression finding a number that is only one character length.

--- main.py
+++ main.py
@@ -55,43 +55,42 @@ def string_decompress(string):
     decompressed_string = ""
     string_length = len(string)
 
-    # Ideally the information should contain letter and number. In example: a3
-    # Check if compression has this format
-    if string_length % 2 != 0:
-        print("The string dosen't have a correct format (letter and number): a3")
-        return decompressed_string
+    # Ideally the format contains letter and a number. Number can be different kinds of length.
+    string_char_index = 0
+    # Iterate through all the string
+    while string_char_index < len(string):
+        current_letter = string[string_char_index]
+        string_char_index += 1
 
-    # Iterate over string by one letter compression information
-    for char_index in range(0, string_length, 2):
-        # Also validate correct format of letter and number: a3
+        # Find the number of times character will be repeated (number). Check if it is more than one character long.
+        number_string = ""
+        while string_char_index < string_length and string[string_char_index].isdigit():
+            number_string += string[string_char_index]
+            string_char_index += 1
 
-        # Find information about one letter
-        current_letter = string[char_index]
-        current_number = string[char_index + 1]
-
-        # Check if compressed string contains defined format
-        if not current_letter.isalpha() or not current_number.isnumeric():
-            print("The string dosen't have a correct format (letter and number): a3")
+        # Check if number is defined
+        if not number_string:
+            print("The string doesn't have a correct format (letter and number): a3")
             print("Decompressed string", decompressed_string)
             return decompressed_string
 
-        # Convert char to int
-        current_number = int(current_number)
-
-        # Add letter defined number of times to decompressed string
+        # Convert number to int and add to decompressed string
+        current_number = int(number_string)
         for _ in range(current_number):
             decompressed_string += current_letter
 
     print("Decompressed string", decompressed_string)
     return decompressed_string
 
-
 def main():
     string_compress("aaabb")
     string_compress("abc")
     string_compress("")
+    string_compress("a")
+    string_compress("a" * 1000)
+    string_compress("ab" * 500)
 
-    string_decompress("a3b2")
+    string_decompress("a10b2")
     string_decompress("a1b1c1")
     string_decompress("")
 
